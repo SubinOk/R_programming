a8edff68efaa9b12d0a033a105fea14863ee959e
SubinOk <subinok130@gm.gist.ac.kr>
2020-09-08 19:07:57 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -25,7 +20,7 @@ cacheSolve <- function(x, ...) {
     return(i)
   }
   data <- x$get()
-  i <- solve(x)
+  i <- solve(data)
   x$setinverse(i)
   i
 }
